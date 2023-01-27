98c6809da42997081f14c8a346588c9b0508427c
haminem <ryuu.shushu@gmail.com>
2023-01-27 14:58:49 +0900

    add 汎用的なコンポネントを追加

---
+++ src/components/layout/Box.tsx
@@ -0,0 +1,5 @@
+import Box, { BoxProps as MuiBoxProps } from "@mui/material/Box";
+
+export type BoxProps = MuiBoxProps;
+
+export default Box;
---
+++ src/layout/Sidebar/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export type SidebarProps = {
+  width: string;
+};
+
+function Sidebar() {
+  return <></>;
+}
+
+export default Sidebar;
